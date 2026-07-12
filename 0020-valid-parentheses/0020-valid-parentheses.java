04823549508498ed5576619b4656d7079bb3a35c
Kulpreet <nikhildhull5652@gmail.com>
2026-07-13 00:00:14 +0530

    Time: 3 ms (87.01%), Space: 43.2 MB (58.02%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(char x:s.toCharArray()){
+            if(x=='('|| x=='[' || x=='{' ){
+                st.push(x);
+            } 
+            else if(x==')' && !st.isEmpty()&& st.peek()=='(') st.pop();
+            else if(x=='}' && !st.isEmpty()&& st.peek()=='{') st.pop();
+            else if(x==']' && !st.isEmpty()&& st.peek()=='[') st.pop();
+            else{
+                return false;
+            }
+        }
+        if(!st.isEmpty()) return false;
+        else return true;
+    }
+}
