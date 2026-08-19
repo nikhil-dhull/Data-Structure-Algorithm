e234040ec5f9bbf5bcb742ebf41b282ca8c76c62
Kulpreet <nikhildhull5652@gmail.com>
2026-08-19 22:03:44 +0530

    Time: 33 ms (29.9%), Space: 54 MB (60.34%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) ->
+    Integer.compare(distance(b), distance(a))
+);
+        for (int[] point:points) {
+            pq.add(point);
+        if (pq.size() > k) {
+            pq.poll();
+            }
+        }
+        int[][] ans = new int[k][2];
+        for (int i=0; i<k; i++) {
+            ans[i]=pq.poll();
+        }
+        return ans;
+    }
+    private int distance(int[] point) {
+        return point[0]*point[0]+point[1]*point[1];
+    }
+}
